04210cc0a4053398fce0891797a832f2a1307010
vasenko1 <igor.va@sbtech.com>
2019-04-20 02:08:14 +0300

    commit2

--- TheBestApplicationEver/SnakeGame/Control.cs
+++ TheBestApplicationEver/SnakeGame/Control.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SnakeGame
 {
     class Control
     {
         bool WallHit = false;
+        char head = 'O';
+        int gameSpeed = 100;
 
         ConsoleKey move = Console.ReadKey().Key;
 
@@ -42,9 +45,27 @@ namespace SnakeGame
                 }
                 Console.SetCursorPosition(x, y);
                 Console.ForegroundColor = ConsoleColor.Green;
+
                 Console.WriteLine("*");
 
+                Console.WriteLine(head);
+                WallHit = DidSnakeHitWall(x, y);
+                if (WallHit)
+                {
+                    ISGameON = false;
+                    Console.SetCursorPosition(30, 20);
+                    Console.WriteLine("Game Over");
+                }
+                if (Console.KeyAvailable) move = Console.ReadKey().Key;
+                Thread.Sleep(gameSpeed);
+
             } while (ISGameON);
+         
+        }
+        private static bool DidSnakeHitWall(int x, int y)
+        {
+            if (x == 1  || x == 60 || y == 0 || y == 26) 
+            return true; return false;
         }
 
 
